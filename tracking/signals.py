aa353ec816f07d01ca413b84e2cec35abe365f69
Wuda2000 <fridawuda@gmail.com>
2025-03-24 10:46:39 +0300

    Update signals.py

--- tracking/signals.py
+++ tracking/signals.py
@@ -1,8 +1,10 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Tracking  # Assuming there is a Tracking model
+from .models import TrackingModel  # Assuming there is a TrackingModel
 
-@receiver(post_save, sender=Tracking)
+
+@receiver(post_save, sender=TrackingModel)
+
 def tracking_post_save(sender, instance, created, **kwargs):
     if created:
         # Logic to execute after a tracking instance is created
